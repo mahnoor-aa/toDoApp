c759cafbaef82673a7377a0b7071f851aae329a7
Mahnoor Ahmad <mahnoorahmad4484@gmail.com>
2025-07-28 12:47:20 +0500

    Test Deployment Pipeline

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -109,7 +109,7 @@ export default function Home() {
 					className="bg-blue-700 text-white px-4 py-2 rounded hover:bg-blue-900 text-center flex font-semibold cursor-pointer"
 				>
 					<IoIosAdd className="text-2xl" />
-					Add Task
+					Delete Task
 				</button>
 			</div>
 
